28f8971f124b46315876f5ec623d049512603329
Purvag Patel <purvag2003@gmail.com>
2016-05-30 16:21:20 -0700

    Reformat countIris

--- src/main/scala/CountIrisApp.scala
+++ src/main/scala/CountIrisApp.scala
@@ -4,27 +4,6 @@
   */
 import org.apache.spark.{SparkConf, SparkContext}
 
-object MapFunction {
-  def mapIris(line: String): (String, Int) = {
-    val elements = line.split(",")
-    var iris = "Unknown"
-    if (elements.length == 5) {
-      iris = elements(4)
-    }
-    return (iris, 1)
-  }
-
-  def sum(ab: (String, Int)): String = ab match {
-    case (a, b) => Array(a, b).mkString(",")
-  }
-}
-
-object ReduceFunction {
-  def sum(val1: Int, val2: Int): Int = {
-    return val1 + val2
-  }
-}
-
 object CountIris {
 
   def main(args: Array[String]) {
@@ -35,9 +14,29 @@ object CountIris {
     val logData = sc.textFile(logFile, 2).cache()
 
     val out = logData.map(MapFunction.mapIris).reduceByKey(ReduceFunction.sum).
-      map(MapFunction.sum)
+      map(MapFunction.csv)
 
-    out.saveAsTextFile("output")
+    out.saveAsTextFile("output/count-iris")
   }
 }
 
+object MapFunction {
+  def mapIris(line: String): (String, Int) = {
+    val elements = line.split(",")
+    var iris = "Unknown"
+    if (elements.length == 5) {
+      iris = elements(4)
+    }
+    return (iris, 1)
+  }
+
+  def csv(ab: (String, Int)): String = ab match {
+    case (a, b) => Array(a, b).mkString(",")
+  }
+}
+
+object ReduceFunction {
+  def sum(val1: Int, val2: Int): Int = {
+    return val1 + val2
+  }
+}
